fa046f8db7f6cde8ba232f503f2717d707568fa1
alanHaidar <alanhb96@hotmail.com>
2023-09-22 16:27:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import './App.css'
+
+const App = () =>{
+
+  return (
+    <>
+      <NavBar/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+
+    return(
+        <header className="header">
+            <h1></h1>
+            <nav className="navbar">
+                <a href="#">link</a>
+                <a href="#">link</a>
+                <a href="#">link</a>
+            </nav>
+        </header>
+    )
+}
+
+
+export default NavBar
